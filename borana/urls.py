1f62c5ab19d271f5c0969bbc12ca8e64c30a3e8b
oromay <orormay@yandex.ru>
2016-12-28 00:54:32 +0300

    tether added

--- borana/urls.py
+++ borana/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.contrib.auth.decorators import login_required
 from django.conf.urls import url, include
 from django.contrib import admin
 from apply.views import done_view, UploadView
